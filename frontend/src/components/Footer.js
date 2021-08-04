5107dcbaf39bdccf7f7c4372b2df0c4e949375da
DanSepulveda <d.sepulveda.perez@gmail.com>
2021-08-04 13:15:31 -0400

    First commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default class Footer extends React.Component{
+    render(){
+        return(
+            <footer className="footer">
+                <p>Made by <span>Daniel Sepúlveda</span> | &copy; All Rights Reserved</p>
+            </footer>
+        )
+    }
+}
+
+// const Footer = () =>{
+//     return (
+//         <footer className="footer">
+//             <p>Made by <span>Daniel Sepúlveda</span> | &copy; All Rights Reserved</p>
+//         </footer>
+//     )
+// }
+
+// export default Footer
