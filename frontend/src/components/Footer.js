d52fee032c900ebab894d44bd9ce0b60e25cde84
DanSepulveda <d.sepulveda.perez@gmail.com>
2021-08-09 07:35:37 -0400

    Version 1.0

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 const Footer = () =>{
     return (
         <footer>
@@ -6,18 +7,17 @@ const Footer = () =>{
                         <h4>Contact<br />Information</h4>
                         <p>Please feel free to contact us through ant of our social media.</p>
                         <div className="social">
-                            <a href='https://www.whatsapp.com' target='_blank'><img className="socialIcon" src='assets/whatsapp.png' alt='Whatsapp Icon' /></a>
-                            <a href='https://www.facebook.com' target='_blank'><img className="socialIcon" src='assets/facebook.png' alt='Facebook Icon' /></a>
-                            <a href='https://www.twitter.com' target='_blank'><img className="socialIcon" src='assets/twitter.png' alt='Twitter Icon' /></a>
-                            <a href='https://www.instagram.com' target='_blank'><img className="socialIcon" src='assets/instagram.png' alt='Instagram Icon' /></a>
+                            <a rel="noreferrer" href='https://www.whatsapp.com' target='_blank'><img className="socialIcon" src='assets/whatsapp.png' alt='Whatsapp Icon' /></a>
+                            <a rel="noreferrer" href='https://www.facebook.com' target='_blank'><img className="socialIcon" src='assets/facebook.png' alt='Facebook Icon' /></a>
+                            <a rel="noreferrer" href='https://www.twitter.com' target='_blank'><img className="socialIcon" src='assets/twitter.png' alt='Twitter Icon' /></a>
+                            <a rel="noreferrer" href='https://www.instagram.com' target='_blank'><img className="socialIcon" src='assets/instagram.png' alt='Instagram Icon' /></a>
                         </div>
                     </div>
                     <div className="footerSection second">
                         <h4>Popular<br />Mytineraries</h4>
                         <ul>
-                            <li>Hiroshima</li>
-                            <li>Kyoto</li>
-                            <li>Nara</li>
+                            <li><a href='#'>Home</a></li>
+                            <li><NavLink to='/cities'>Cities</NavLink></li>
                         </ul>
                     </div>
                     <div className="footerSection third" style={{backgroundImage: "url('/assets/logo.png')"}}>
--- frontend/src/components/main/CallToAction.js
+++ frontend/src/components/main/CallToAction.js
@@ -1,14 +1,16 @@
 import { NavLink } from 'react-router-dom'
 const CallToAction = () =>{
     return (
-        <section id='call' className="callToAction" style={{backgroundImage: "url('assets/call.png')"}}>
+        <section id='call' className="callToAction">
             <article>
-                <div className="callText">
-                    <h2>There is nothing to worry about.</h2>
-                    <h3>You are one step closer for making your dreams come true!</h3>
-                </div>
-                <div>
-                    <NavLink to='/cities' className="without"><div className="button"><span>Start Now</span></div></NavLink>
+                <img src='/assets/ninja.png' alt='Ninja Icon' className='ninja' />
+                <div className='textContainer'>
+                    <div className="callText">
+                        <h2>There is nothing to worry about.</h2>
+                        <h3>You are one step closer for making your dreams come true!</h3>
+                        <h3>Choose your destination.</h3>
+                        <NavLink to='/cities' className="button">Start Now</NavLink>
+                    </div>
                 </div>
             </article>
         </section>
