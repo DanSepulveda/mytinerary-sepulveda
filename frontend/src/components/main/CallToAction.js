9bfcd1abb2abc32704be3f75d01c4587c55e95d9
DanSepulveda <d.sepulveda.perez@gmail.com>
2021-08-08 22:03:16 -0400

    ajustes css

--- frontend/src/components/main/CallToAction.js
+++ frontend/src/components/main/CallToAction.js
@@ -1,18 +1,15 @@
 import { NavLink } from 'react-router-dom'
 const CallToAction = () =>{
     return (
-        <section id='call' className="callToAction">
-            <article className="bambooImg">
-                <div className="bamboo"></div>
-                <div className="hola">
+        <section id='call' className="callToAction" style={{backgroundImage: "url('assets/call.png')"}}>
+            <article>
+                <div className="callText">
                     <h2>There is nothing to worry about.</h2>
                     <h3>You are one step closer for making your dreams come true!</h3>
                 </div>
-                <div className="flag">
-                    <NavLink to='/cities' className="without"><div className="button"><span>Start<br />Now</span></div></NavLink>
+                <div>
+                    <NavLink to='/cities' className="without"><div className="button"><span>Start Now</span></div></NavLink>
                 </div>
-                <div className="nada"></div>
-                <div className="panda"></div>
             </article>
         </section>
     )
