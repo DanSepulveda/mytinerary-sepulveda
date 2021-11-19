02589f7667e913b203dcf81606d83d544227345c
DanSepulveda <d.sepulveda.perez@gmail.com>
2021-11-19 13:02:53 -0300

    Routes edited

--- frontend/src/redux/actions/citiesActions.js
+++ frontend/src/redux/actions/citiesActions.js
@@ -1,22 +1,21 @@
 import axios from "axios";
+const HOST = 'http://localhost:4000'
 
 const citiesActions = {
   getList: () => {
     return async (dispatch) => {
-      let response = await axios.get("http://localhost:4000/api/cities");
+      let response = await axios.get(`${HOST}/api/cities`);
       if (!response.data.success) {
         throw new Error("Backend - DB Error");
       }
       dispatch({ type: "GET_ALL_CITIES", payload: response.data.response });
     };
   },
-
   getFilteredList: (search) => {
     return (dispatch) => {
       dispatch({ type: "GET_FILTERED_CITIES", payload: search });
     };
   },
-
   getOneCity: (id) => {
     return (dispatch) => {
       dispatch({ type: "GET_ONE_CITY", payload: id });
